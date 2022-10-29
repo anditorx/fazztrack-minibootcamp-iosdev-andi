75cdee4131f0efe0741f3fc99baf9caddf36aeb1
Andi Rustianto <anditodev46@gmail.com>
2022-10-29 23:29:33 +0700

    [ADD] Pertemuan 14

---
+++ 14-Working-with-Core-Data/PodcastsApp/PodcastsApp/Modules/Podcast/PodcastsViewController.swift
@@ -0,0 +1,92 @@
+import UIKit
+import Kingfisher
+
+class PodcastsViewController: BaseViewController {
+    @IBOutlet weak var tableView: UITableView!
+    let searchController = UISearchController(searchResultsController: nil)
+    var searchTime: Timer?
+    
+    let viewModel = PodcastsViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setup()
+    }
+    
+    func setup() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        // MARK: Search Bar
+        definesPresentationContext = true
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+        
+        
+    }
+    
+    func searchPodcasts(q: String) {
+        searchTime?.invalidate()
+        searchTime = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { [weak self] (timer) in
+            self?.viewModel.searchPodcasts(q: q) { [weak self] (result) in
+                self?.tableView.reloadData()
+            }
+        })
+    }
+}
+
+// MARK: - UISearchBarDelegate
+extension PodcastsViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if let text = searchBar.text, text.count >= 3 {
+            searchPodcasts(q: text)
+        }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if let text = searchBar.text, text.count >= 3 {
+            searchPodcasts(q: text)
+        }
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension PodcastsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfPodcasts
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "podcastCellId", for: indexPath) as! PodcastViewCell
+        
+        let index = indexPath.row
+        cell.nameLabel.text = viewModel.podcastTrackName(at: index)
+        cell.artistLabel.text = viewModel.podcastArtistName(at: index)
+        cell.episodeLabel.text = viewModel.podcastTrackCount(at: index)
+        cell.thumbImageView.kf.setImage(with: URL(string: viewModel.podcastImagUrl(at: index))) { (result) in
+            switch result {
+            case.success:
+                cell.thumbImageView.contentMode = .scaleAspectFill
+                
+            case.failure:
+                cell.thumbImageView.contentMode = .center
+                cell.thumbImageView.image = UIImage(systemName: "photo")
+                
+            }
+        }
+        
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension PodcastsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+   
+}
